a7f5ac7b6e072f31a6a846b0519e02d0a682a97a
Shoaib <shoaibshoukatbhatti99@gmail.com>
2024-03-09 16:49:58 +0500

    add statifiles and host headers

--- core/core/settings.py
+++ core/core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7m6cbm)kj2t(-qr#90nuw+=3@nxuj!+%90-jy(nnf=*vgf5fd)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shoaibshoukat987.pythonanywhere.com']
 
 
 # Application definition
@@ -122,6 +122,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
