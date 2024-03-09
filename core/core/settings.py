6c5140a4e54916fabc2813bedb3ba40d10ac272e
Shoaib <shoaibshoukatbhatti99@gmail.com>
2024-03-09 23:45:14 +0500

    Add Responsiveness

--- core/core/settings.py
+++ core/core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7m6cbm)kj2t(-qr#90nuw+=3@nxuj!+%90-jy(nnf=*vgf5fd)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shoaibshoukat987.pythonanywhere.com']
 
 
 
